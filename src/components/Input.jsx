196899b0695fc972f5b526f935f0cd020ab3d1d4
LuisThiago858 <luisrabello.ltsr@gmail.com>
2024-01-29 23:33:00 -0400

    V 0.0.6

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useState , useCallback} from 'react';
 
 export default function InputTextBin(){
+    //visualizar o valor e alterar o valor
+    const [value, setValue] = useState("")
+    const [isValid, setIsValid] = useState(false)
+
+    const catchText =useCallback((event, input) => {
+        
+        setValue(event.target.value);
+        
+    }, []);
+    if(setValue.length>8){
+        setIsValid(true)
+    }
+
+
     return (
-        <input type={'text'} className={'content-input'} onChange={catchText} placeholder="Digite um numero binário"></input>
+        <div>
+            <input type={'text'} className={'content-input'} onChange={catchText} placeholder="Digite um numero binário" />
+            <p>{value}</p>
+        </div>
+        //
     );
 }
 
+/*onChange={catchText}*/
